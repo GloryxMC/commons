8c46175478f98fad7bdf5f6e6cdea99ad59055ca
ilya <ilya@gloryx.net>
2022-09-06 20:59:51 +0300

    fastmap

--- core/src/main/kotlin/net/gloryx/commons/kotlinlove/fastconv.kt
+++ core/src/main/kotlin/net/gloryx/commons/kotlinlove/fastconv.kt
@@ -5,3 +5,5 @@ val Number.i get() = toInt()
 val Number.l get() = toLong()
 val Number.f get() = toFloat()
 
+fun <T, R> T.fastMap(vararg maps: Pair<T, R>) = fastMap(maps.toMap())
+fun <T, R> T.fastMap(map: Map<T, R>) = map[this]
